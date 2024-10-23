1527d2225487de1b9b62ca288fec792a681382d4
sheharyar.raza@gmail.com <sheahryar.raza@gmail.com>
2024-10-22 22:42:41 -0400

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' @title Print hello world
+#' @description Print hello world
+#' @author Sheahryar Raza
+#' @param none description
+#'
+#'
+
+
+hello <- function() {
+  print("Hello, world!")
+}
---
+++ R/toydata.R
@@ -0,0 +1,18 @@
+#' Example Data
+#' Data on toys
+#'
+#' @docType data
+#' @format A dataframe
+#' \describe{
+#'  \item{toyid}{Toy ID}
+#'  \item{toyname}{Toy Name}
+#'  \item{price}{Price of the toy}
+#'  \item{age}{Age group for the toy}
+#'  }
+#' @source Aya
+#' @references My reference
+#' @keywords datasets
+
+"toydata"
+
+
